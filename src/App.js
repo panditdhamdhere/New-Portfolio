b3ad3e82ef749e8dc83164a59ef784dec50bd0ac
panditdhamdhere <dhamdherepandit@gmail.com>
2023-02-22 09:22:27 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+
+const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div>
-      <Header />
+    <div className={darkMode && "dark"}>
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { MdNightsStay, MdWbSunny } from 'react-icons/md'
 
-const Header = () => {
+const Header = ({ darkMode, setDarkmode }) => {
     return (
         <Header className='bg-white dark:bg-gray-900 text-gray-900 dark:text-white'>
             <nav className='flex justify-between items-center p-5 '>
                 <h1 className='text-lg'>Pandit</h1>
             </nav>
-            <div>
-                <MdNightsStay className='text-2xl cursor-pointer' />
-                <MdWbSunny className='text-2xl cursor-pointer'/>
+            <div onClick={() => setDarkmode(!darkMode)}>
+                {
+                    darkMode ? (
+                        <MdWbSunny className='text-2xl cursor-pointer' />
+                    ) : (
+                        <MdNightsStay className='text-2xl cursor-pointer' />
+                    )
+                }
+
+
             </div>
         </Header>
     )
